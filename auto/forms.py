ffffefdfe1cee5737434307f23e38966e7f328e1
TheJonry <110304328+TheJonry@users.noreply.github.com>
2024-01-23 03:52:30 -0500

    form updates

---
+++ auto/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.forms import ModelForm
+from django.forms.fields import Field
+from auto.models import *
+
+class OmnicellForm(ModelForm):
+    class Meta:
+        model = Omnicell
+        fields = ['Serial_Number', 'Omni_Id', 'Omni_Description', 'Emergency', 'Model', 'CT_Version', 'PC_Name', 'Ivanti', 'Site', 'Building', 'Area', 'Room']
+        widgets = {
+            'Omni_Id': forms.TextInput(attrs={'class': 'inputField'}),
+        }
--- auto/views.py
+++ auto/views.py
@@ -1,7 +1,10 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
+from django.contrib import messages
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from auto.forms import *
 from .models import *
@@ -22,3 +25,16 @@ class OmnicellList(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+    
+class OmnicellUpdate(UpdateView):
+    model = Omnicell
+    template_name = 'omnicell_update.html'
+    form_class = OmnicellForm
+    
+    def form_valid(self, form):
+        messages.success(self.request, "Omnicell Updated")
+        return super(OmnicellUpdate, self).form_valid(form)
+    
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy("omnicell", kwargs={'pk': pk})

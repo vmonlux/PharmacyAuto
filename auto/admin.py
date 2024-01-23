40816721eeca19eb665465fff0f45ada0595fab0
TheJonry <110304328+TheJonry@users.noreply.github.com>
2024-01-23 02:23:20 -0500

    admin updates

---
+++ auto/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from auto.models import *
+from auto.forms import *
+
+admin.site.register(Omnicell)
+admin.site.register(Refrigerator)
+admin.site.register(OmnicellModel)
+admin.site.register(Site)
+admin.site.register(Building)

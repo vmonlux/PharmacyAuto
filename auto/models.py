8b771db55137b8dd64cf863ab17003697296d890
TheJonry <110304328+TheJonry@users.noreply.github.com>
2024-01-23 01:42:08 -0500

    model additions

--- auto/models.py
+++ auto/models.py
@@ -37,6 +37,37 @@ class Refrigerator(models.Model):
     def __str__(self):
         return self.id
 
+class OmnicellModel(models.Model):
+    """Class for storing Omnicell Models """
+
+    #Fields
+    id = models.BigAutoField(primary_key=True)
+    Generation = models.CharField(max_length=5, blank=True, null=True)
+    Model = models.CharField(max_length=50, blank=True, null=True)
+
+    # Metadata
+    class Meta:
+        ordering = ['Generation', 'Model']
+    
+    # Methods
+    def __str__(self):
+        return str(self.Generation+" "+self.Model)
+    
+class Site(models.Model):
+    """ Class for storing Sites """
+
+    #Fields
+    id = models.BigAutoField(primary_key=True)
+    Name = models.CharField(max_length=50, blank=True, null=True)
+
+    # Metadata
+    class Meta:
+        ordering = ['Name']
+    
+    # Methods
+    def __str__(self):
+        return self.Name
+
 # class theclass(models.Model):
 #     """Class Description"""
 

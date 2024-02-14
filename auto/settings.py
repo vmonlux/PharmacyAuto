b5ed4357040ccc31e5199ba82574e145097b8de8
TheJonry <110304328+TheJonry@users.noreply.github.com>
2024-02-14 02:54:35 -0500

    rebase

--- auto/settings.py
+++ auto/settings.py
@@ -106,6 +106,8 @@ elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'auto.User'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
